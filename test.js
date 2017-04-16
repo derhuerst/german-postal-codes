26703962250206c071502aea456ebfdb57fbca15
Jannis R <mail@jannisr.de>
2017-04-16 14:02:50 +0200

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,34 @@
 'use strict'
 
 const test = require('tape')
+const uniq = require('lodash.uniq')
 
 const codes = require('.')
 
-test('codes', (t) => {
+test('codes should be in a sorted array', (t) => {
+	t.plan(2)
+
+	t.ok(Array.isArray(codes))
+	t.deepEqual(codes, codes.sort())
+})
+
+test('codes should be unique', (t) => {
+	t.plan(1)
+
+	t.deepEqual(codes, uniq(codes))
+})
+
+test('codes should look like valid PLZs', (t) => {
+	t.plan(codes.length)
+	const validPLZ = /^\d{5}$/
+
+	for (let code of codes) {
+		t.ok(validPLZ.test(code), code + ' is invalid')
+	}
+})
+
+test('there should be more than 5k codes', (t) => {
+	t.plan(1)
+
+	t.ok(codes.length > 5000, `only ${codes.length} codes`)
 })
